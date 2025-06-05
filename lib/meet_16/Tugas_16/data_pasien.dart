a0a21651892864b18815a09c6d80f660d03e898d
AnwarSeptian <anwarpalmer26@gmail.com>
2025-06-05 21:40:02 +0700

    update tugas 11 Done

--- lib/meet_16/Tugas_16/data_pasien.dart
+++ lib/meet_16/Tugas_16/data_pasien.dart
@@ -14,6 +14,7 @@ class _DataPasienState extends State<DataPasien> {
   final TextEditingController umurController = TextEditingController();
   final TextEditingController kondisiController = TextEditingController();
   List<ModelTugas> daftarPasien = [];
+  final _formkey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -29,18 +30,20 @@ class _DataPasienState extends State<DataPasien> {
   }
 
   Future<void> simpanData() async {
-    final nama = namaController.text;
-    final umur = int.tryParse(umurController.text) ?? 0;
-    final kondisi = kondisiController.text;
+    if (_formkey.currentState!.validate()) {
+      final nama = namaController.text;
+      final umur = int.tryParse(umurController.text) ?? 0;
+      final kondisi = kondisiController.text;
 
-    if (nama.isNotEmpty && kondisi.isNotEmpty && umur > 0) {
-      await DbTugas16.insertTugas(
-        ModelTugas(nama: nama, umur: umur, kondisi: kondisi),
-      );
-      namaController;
-      umurController;
-      kondisiController;
-      muatData();
+      if (nama.isNotEmpty && kondisi.isNotEmpty && umur > 0) {
+        await DbTugas16.insertTugas(
+          ModelTugas(nama: nama, umur: umur, kondisi: kondisi),
+        );
+        namaController.clear();
+        umurController.clear();
+        kondisiController.clear();
+        muatData();
+      }
     }
   }
 
@@ -56,100 +59,123 @@ class _DataPasienState extends State<DataPasien> {
         backgroundColor: Color(0xffAF3E3E),
         leading: Icon(Icons.medical_services_rounded),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Color(0xffEAEBD0),
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  children: [
-                    Text(
-                      "Masukkan Kondisi Pasien",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: namaController,
-                      decoration: InputDecoration(
-                        labelText: "Nama",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(24),
+      body: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Color(0xffEAEBD0),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Masukkan Kondisi Pasien",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 12),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Lengkapi data pasien";
+                          }
+                          return null;
+                        },
+                        controller: namaController,
+                        decoration: InputDecoration(
+                          labelText: "Nama",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 12),
+                      SizedBox(height: 12),
 
-                    TextField(
-                      controller: umurController,
-                      decoration: InputDecoration(
-                        labelText: "Umur",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(24),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Lengkapi data pasien";
+                          }
+                          return null;
+                        },
+                        controller: umurController,
+                        decoration: InputDecoration(
+                          labelText: "Umur",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 12),
-                    TextField(
-                      controller: kondisiController,
-                      decoration: InputDecoration(
-                        labelText: "Kondisi",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(24),
+                      SizedBox(height: 12),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Lengkapi data pasien";
+                          }
+                          return null;
+                        },
+                        controller: kondisiController,
+                        decoration: InputDecoration(
+                          labelText: "Kondisi",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
                         ),
                       ),
-                    ),
 
-                    SizedBox(height: 16),
-                    ElevatedButton(
-                      onPressed: simpanData,
-                      child: Text("Simpan"),
-                    ),
-                  ],
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          simpanData();
+                        },
+                        child: Text("Simpan"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 12),
+            SizedBox(height: 12),
 
-          Row(
-            children: [
-              Expanded(child: Divider(thickness: 4, endIndent: 8)),
-              Text(
-                "Data Pasien",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Expanded(child: Divider(thickness: 4, indent: 8)),
-            ],
-          ),
-          SizedBox(height: 12),
-          Expanded(
-            child: Container(
-              color: Color(0xffAF3E3E),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: ListView.builder(
-                  itemCount: daftarPasien.length,
-                  itemBuilder: (context, index) {
-                    final tugas = daftarPasien[index];
-                    return Card(
-                      child: ListTile(
-                        leading: CircleAvatar(child: Text("${tugas.id}")),
-                        title: Text(tugas.nama),
-                        subtitle: Text("Umur : ${tugas.umur}"),
-                        trailing: Text(
-                          "Kondisi :${tugas.kondisi}",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+            Row(
+              children: [
+                Expanded(child: Divider(thickness: 4, endIndent: 8)),
+                Text(
+                  "Data Pasien",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Expanded(child: Divider(thickness: 4, indent: 8)),
+              ],
+            ),
+            SizedBox(height: 12),
+            Expanded(
+              child: Container(
+                color: Color(0xffAF3E3E),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: ListView.builder(
+                    itemCount: daftarPasien.length,
+                    itemBuilder: (context, index) {
+                      final tugas = daftarPasien[index];
+                      return Card(
+                        child: ListTile(
+                          leading: CircleAvatar(child: Text("${tugas.id}")),
+                          title: Text(tugas.nama),
+                          subtitle: Text("Umur : ${tugas.umur}"),
+                          trailing: Text(
+                            "Kondisi :${tugas.kondisi}",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
