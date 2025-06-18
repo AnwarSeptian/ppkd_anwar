f21c9ed558c8af0316fc7f07015ad85b90533e97
AnwarSeptian <anwarpalmer26@gmail.com>
2025-06-19 00:47:16 +0700

    update fitur search

--- lib/tugas14/models/user_model.dart
+++ lib/tugas14/models/user_model.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final dataUser = dataUserFromJson(jsonString);
-
 import 'dart:convert';
 
 List<DataUser> dataUserFromJson(String str) =>
--- lib/tugas14/view/home_page.dart
+++ lib/tugas14/view/home_page.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:ppkd_anwar/constant/app_color.dart';
 import 'package:ppkd_anwar/tugas14/api/get_user.dart';
+import 'package:ppkd_anwar/tugas14/models/user_model.dart';
 import 'package:ppkd_anwar/tugas14/view/detail_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<DataUser> semuaUser = [];
+  List<DataUser> hasilSearch = [];
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final data = await getUser();
+    setState(() {
+      semuaUser = data;
+      hasilSearch = data;
+    });
+  }
+
+  void searchUser(String query) {
+    final hasil =
+        semuaUser.where((user) {
+          return user.fullName.toLowerCase().contains(query.toLowerCase());
+        }).toList();
+    setState(() {
+      hasilSearch = hasil;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: AppColor.hitam1,
-
-      //   centerTitle: true,
-      //   actions: [
-
-      //   ],
-      // ),
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: Expanded(
           child: Column(
             children: [
               TextFormField(
+                onChanged: searchUser,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: AppColor.cream2,
@@ -79,83 +104,88 @@ class HomePage extends StatelessWidget {
               ),
               SizedBox(height: 12),
 
-              FutureBuilder(
-                future: getUser(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasData) {
-                    final users = snapshot.data;
-                    return GridView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: users?.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 4,
-                        mainAxisSpacing: 4,
-                        // childAspectRatio: 1,
-                      ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: FutureBuilder(
+                    future: getUser(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasData) {
+                        final users = snapshot.data;
+                        return GridView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: hasilSearch.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                crossAxisSpacing: 4,
+                                mainAxisSpacing: 4,
+                                // childAspectRatio: 1,
+                              ),
 
-                      itemBuilder: (BuildContext context, int index) {
-                        final user = users?[index];
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder:
-                                    (context) => DetailPage(
-                                      nama: user?.firstName,
-                                      namaLengkap: user?.fullName,
-                                      image: user?.imageUrl,
-                                      title: user?.title,
-                                      family: user?.family,
+                          itemBuilder: (BuildContext context, int index) {
+                            final user = hasilSearch[index];
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder:
+                                        (context) => DetailPage(
+                                          nama: user.firstName,
+                                          namaLengkap: user.fullName,
+                                          image: user.imageUrl,
+                                          title: user.title,
+                                          family: user.family,
+                                        ),
+                                  ),
+                                );
+                              },
+                              child: Stack(
+                                alignment: Alignment.bottomCenter,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Image.network(
+                                      "${user?.imageUrl}",
+                                      fit: BoxFit.cover,
+                                      width: double.infinity,
+                                      height: double.infinity,
                                     ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(4),
+                                      ),
+                                      color: AppColor.cream1,
+                                    ),
+                                    height: 32,
+                                    width: double.infinity,
+                                    child: Center(
+                                      child: Text(
+                                        "${user?.fullName}",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                          color: AppColor.hitam2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             );
                           },
-                          child: Stack(
-                            alignment: Alignment.bottomCenter,
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(8),
-                                child: Image.network(
-                                  "${user?.imageUrl}",
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                  height: double.infinity,
-                                ),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(4),
-                                  ),
-                                  color: AppColor.cream1,
-                                ),
-                                height: 32,
-                                width: double.infinity,
-                                child: Center(
-                                  child: Text(
-                                    "${user?.fullName}",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      color: AppColor.hitam2,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
                         );
-                      },
-                    );
-                  } else {
-                    return Text("Error : $snapshot");
-                  }
-                },
+                      } else {
+                        return Text("Error : $snapshot");
+                      }
+                    },
+                  ),
+                ),
               ),
             ],
           ),
